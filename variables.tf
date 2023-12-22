494e42c4821cd56b239d6d79d7920e3c4c1f3d18
vartika0709 <87943568+vartika0709@users.noreply.github.com>
2023-12-22 13:56:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,4 @@
-variable "useranme1" {
-  type = string
-  description = "secret username"
-  }
-variable "password1" {
-  type = string
-  description = "secret password"
-  }
+variable "example" {
+  default = {}
+  type = map(string)
+}
